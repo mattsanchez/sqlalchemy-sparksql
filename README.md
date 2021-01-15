f3ad9ac91652631c39777c2bff7f5760cdf4bd09
Matt Sanchez <msanchez@cognitivescale.com>
2021-01-15 11:40:40 -0600

    Initial import

---
+++ README.md
@@ -0,0 +1,2 @@
+# SparkSQL for SQLAlchemy
+This library extends PyHive with support for the SparkSQL flavor of Apache Hive. 
---
+++ setup.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+import sqlalchemy_sparksql
+import sys
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
+
+with open('README.md') as readme:
+    long_description = readme.read()
+
+setup(
+    name="SparkSQL Driver for SQLAlchemy",
+    version=sqlalchemy_sparksql.__version__,
+    description="SparkSQL Driver for SQLAlchemy",
+    long_description=long_description,
+    url='https://github.com/matthsanchez/sqlalchemy-sparksql',
+    author="Matt Sanchez",
+    author_email="matt at c12.com",
+    license="Apache License, Version 2.0",
+    packages=['pyhive', 'TCLIService'],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Topic :: Database :: Front-Ends",
+    ],
+    install_requires=[
+        'sqlalchemy>=0.12.0',
+        'pyhive[hive]',
+    ],
+    tests_require=[
+        'mock>=1.0.0',
+        'pytest',
+        'pytest-cov',
+        'requests>=1.0.0',
+        'sasl>=0.2.1',
+        'sqlalchemy>=0.12.0',
+        'thrift>=0.10.0',
+    ],
+    cmdclass={'test': PyTest},
+    package_data={
+        '': ['*.rst'],
+    },
+    entry_points={
+        'sqlalchemy.dialects': [
+            'sparksql = sqlalchemy_sparksql.sparksql:SparkSqlDialect',
+        ],
+    }
+)
